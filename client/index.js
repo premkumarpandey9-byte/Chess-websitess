ba505f077d50f6bdd3d11e02b12e86284726f981
premkumarpandey9-byte <premkumarpandey9@gmail.com>
2025-10-24 15:07:06 +0530

    Create index.js

---
+++ client/index.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Mini Lichess Clone</title>
+    <link rel="stylesheet" href="/client/styles.css" />
+  </head>
+  <body>
+    <div id="root"></div>
+    <script src="/socket.io/socket.io.js"></script>
+    <script type="module" src="/client/index.js"></script>
+  </body>
+</html>
